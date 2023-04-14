5be9900c0d8e2d618653d1261ff1d3a3e404ca4b
Matheus de Oliveira <matheus_oliveira8@estudante.sc.senai.br>
2023-04-14 15:19:57 -0300

    takeUntil e DebounceTime

--- public/app/app.js
+++ public/app/app.js
@@ -2,11 +2,20 @@ import { handleStatus } from './utils/promise-helpers.js';
 import { log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
+
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
+);
+
+const action = operations(() =>
+    service
+        .sumItems('2143')
+        .then(console.log)
+        .catch(console.log)
+)
 
 document
     .querySelector('#myButton')
-    .onclick = () => 
-        service
-        .sumItems('2143')
-        .then(console.log)
-        .catch(console.log);
+    .onclick = action;
